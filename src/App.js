6ddb97d9406c086073227ae3850c1d974dc0b5ec
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-11-16 08:03:23 -0300

    initial config

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+function App(){
+
+    return (
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="sign-up" element={<SignUp/>}/>
+                <Route path="sign-in" element={<SignIn/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
