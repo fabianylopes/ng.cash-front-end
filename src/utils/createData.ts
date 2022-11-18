49df4da1641c8784f08835502df58d363d8cbad3
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-11-18 07:20:25 -0300

    create db data

--- src/utils/createData.ts
+++ src/utils/createData.ts
@@ -1,4 +1,6 @@
-import { Session, User } from "@prisma/client";
+import { Account, Session, Transaction, User } from "@prisma/client";
 
 export type CreateUser = Omit<User, "id">
 export type CreateSession = Omit<Session, "id">
+export type CreateAccount = Omit<Account, "id">
+export type CreateTransactions = Omit<Transaction, "id">
