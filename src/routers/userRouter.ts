36ac3fc165422846f6203a91eac4228a08010854
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-11-18 08:55:25 -0300

    feat: sign up schemas

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-import * as userController from "../controllers/userController.js"
+import * as userController from "../controllers/userController.js";
+import validateSchemaMiddleware from "../middlewares/schemaMiddleware.js";
+import userSchema from "../schemas/ userSchema.js";
 
 const userRouter = Router();
 
-userRouter.post('/sign-up', userController.SignUp)
-userRouter.post('/sign-in', userController.SignIn)
-
+userRouter.post('/sign-up', validateSchemaMiddleware(userSchema), userController.SignUp);
+userRouter.post('/sign-in', userController.SignIn);
 
 export default userRouter;
--- src/schemas/ userSchema.ts
+++ src/schemas/ userSchema.ts
@@ -2,8 +2,8 @@ import joi from "joi";
 import { CreateUser } from "../utils/createData.js";
 
 const userSchema = joi.object<CreateUser>({
-    username: joi.string().required(),
-    password: joi.string().required()
+    username: joi.string().min(3).required(),
+    password: joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/).required()
 });
 
 export default userSchema;
