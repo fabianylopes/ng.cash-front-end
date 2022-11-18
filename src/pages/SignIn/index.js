afdc753e1fe4f98d4c2c6beccd31bf57c6a3f730
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-11-18 10:20:26 -0300

    sign in flow

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,35 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
 import { Container, Form, Input, Button, Link } from "./style";
 import Logo from '../../assets/logo.jpg';
+import api from "../../services/api";
 
 export default function SignIn() {
   const navigate = useNavigate();
 
+  const [signInData, setSignInData] = useState({});
+
+  function handleSignIn(e){
+    e.preventDefault();
+
+    api.signIn(signInData).then(() => navigate('/')).catch(handleFailure)
+  }
+
+  function handleFailure(error){
+    alert(`${error}!\nPreencha os campos corretamente!`);
+    setSignInData({});
+  }
+
   return (
     <Container>
       <img src={Logo} alt="logo"/>
-      <Form>
+      <Form onSubmit={handleSignIn}>
         <Input 
         type="text" 
         placeholder="username"
+        value={signInData.username || ''}
+        onChange={e => setSignInData({...signInData, username: e.target.value})}
         required
         >
         </Input>
@@ -20,14 +37,14 @@ export default function SignIn() {
         <Input 
         type="password" 
         placeholder="password"
+        value={signInData.password || ''}
+        onChange={e => setSignInData({...signInData, password: e.target.value})}
         required
         >         
         </Input>
-
         <Button type="submit">Sign In</Button>
       </Form>
-
-        <Link onClick={() => navigate('/sign-up')}>Not an User? Create account</Link>
+      <Link onClick={() => navigate('/sign-up')}>Not an User? Create account</Link>
     </Container>
-  )
+  );
 }
