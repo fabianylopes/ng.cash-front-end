b8c8b30693b0e78b79a063e68a1d4f3c0001623d
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-11-18 15:22:29 -0300

    feat: sign in persistence

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
 
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import UserContext from "./contexts/UserContext";
 
 function App(){
 
-    return (
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken);
 
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="sign-up" element={<SignUp/>}/>
-                <Route path="sign-in" element={<SignIn/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <UserContext.Provider value={{ token, setToken }}>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="sign-up" element={<SignUp/>}/>
+              <Route path="sign-in" element={<SignIn/>}/>
+          </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,38 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 
 import { Container, Form, Input, Button, Link } from "./style";
 import Logo from '../../assets/logo.jpg';
 import api from "../../services/api";
+import UserContext from "../../contexts/UserContext";
 
 export default function SignIn() {
   const navigate = useNavigate();
 
+  const { token, setToken } = useContext(UserContext);
+
   const [signInData, setSignInData] = useState({});
+
+  useEffect(() => {
+    if(token){
+      navigate('/');
+    }
+  }, [token]); // eslint-disable-line react-hooks/exhaustive-deps
 
   function handleSignIn(e){
     e.preventDefault();
 
-    api.signIn(signInData).then(() => navigate('/')).catch(handleFailure)
+    api.signIn(signInData).then(handleSuccess).catch(handleFailure)
   }
+
+  function handleSuccess(response){
+    setToken(response.data.token);
+
+    localStorage.setItem('token', response.data.token);
+    
+    navigate('/')
+  }
+  console.log(token);
 
   function handleFailure(error){
     alert(`${error}!\nPreencha os campos corretamente!`);
